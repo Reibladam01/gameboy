a125637da97beaaafe374c7a873ae8ba4c9f2f9d
Reibladam01 <72592729595@szily.hu>
2025-02-24 12:19:34 +0100

    games.php létrehozva

---
+++ games.php
@@ -0,0 +1,33 @@
+<?php
+require_once("dbvezerlo.php");
+
+class Games{
+    private $games = [];
+
+    public function __construct(){
+        $query = "SELECT g_ID, name, pic FROM Games";
+        $dbvez = new DBVezerlo();
+        $this->games = $dbvez->executeSelectQuery($query);
+        $dbvez->closeDB();
+    }
+
+    public function getAllGames(){
+        return $this->games;
+    }
+
+    public function getGamesById($GameId){
+        $query = "SELECT g_ID, name, pic FROM Games WHERE g_ID =".$GameId;
+        $dbvez = new DBVezerlo();
+        $this->games = $dbvez->executeSelectQuery($query);
+        $dbvez->closeDB();
+        return $this->games;
+    }
+
+    public function getGamesByType($TypeName){
+        $query = "SELECT g_ID, name, pic FROM Games INNER JOIN type ON Games.type_ID=type.t_id WHERE type.t_name= '".$TypeName."'";
+        $dbvez = new DBVezerlo();
+        $this->games = $dbvez->executeSelectQuery($query);
+        $dbvez->closeDB();
+        return $this->games;
+    }
+}
